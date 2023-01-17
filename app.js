49a21f89e11972caff14d63e044b30af8a1d919f
AnastasiaThunderfoot <68298206+AnastasiaTkachenko@users.noreply.github.com>
2023-01-17 17:20:10 +0100

    JS getUsers

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 /* Globals  */
+const todoList = document.getElementById("todo-list");
 let todos = [];
 let users = [];
 
 /* Attach Events */
 document.addEventListener("DOMContentLoaded", itinApp);
 
+/* Basic logic  */
+function getUserName(userId) {
+  const user = users.find((u) => u.i === userId);
+  return user.name;
+}
+function printTodo({ id, userId, title, complete }) {
+  const li = document.createElement("li");
+  li.className = "todo-item";
+  li.dataset.id = id;
+  li.innerText = `<span>${title} by <b>${getUserName(userId)}</b>`;
+
+  todoList.prepend(li);
+
+}
+
 /* Event logic  */
 function initApp() {
   PromiseAll([getAllToDos(), getAllToDos()]).then((values) => {
     [todos, users] = values;
     // send it to the page, draw them on the page
+    todos.forEach((todo) => printTodo(todo));
   });
 }
 
